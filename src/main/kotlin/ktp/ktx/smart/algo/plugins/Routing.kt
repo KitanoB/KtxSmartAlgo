354805b9d7d35ee896fd9f62370b6cc6488d7c89
KitanoB <kitanoblog@proton.me>
2023-12-28 20:35:35 +0700

    Mininal Routing ok - receive the json

--- src/main/kotlin/ktp/ktx/smart/algo/plugins/Routing.kt
+++ src/main/kotlin/ktp/ktx/smart/algo/plugins/Routing.kt
@@ -1,13 +1,29 @@
 package ktp.ktx.smart.algo.plugins
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+import ktp.ktx.smart.algo.data.model.AlgoTest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 fun Application.configureRouting() {
+
+    val logger: Logger = LoggerFactory.getLogger(javaClass)
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        post("/test") {
+            val algo: AlgoTest = call.receive<AlgoTest>()
+            val jsonStr = Json.encodeToString(AlgoTest.serializer(), algo)
+            logger.info("============================================")
+            logger.info("Received POST /test with payload: $jsonStr")
+            call.respond(HttpStatusCode.OK, algo)
         }
     }
 }
